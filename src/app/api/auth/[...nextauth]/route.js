990ddb1f1404aef30eba4214a75c79878c9e8e50
Arjun kumar vs <arjun.tech177@gmail.com>
2024-05-16 01:05:17 +0530

    Added mini profile to the sidebar

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -7,7 +7,14 @@ const handler = NextAuth({
         clientId: process.env.GOOGLE_ID,
         clientSecret: process.env.GOOGLE_SECRET
     })
-  ]
+  ],
+  callbacks: {
+    async session({session, token}){
+        session.user.username = session.user.name.split(' ').join('').toLocaleLowerCase();
+        session.user.uuid = token.sub
+        return session;
+    }
+  }
 })
 
 export { handler as GET, handler as POST };
--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -3,13 +3,15 @@ import { signIn, signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
 import React from 'react'
 import { FaXTwitter } from "react-icons/fa6";
-import { HiHome } from "react-icons/hi";
+import { HiHome, HiDotsHorizontal } from "react-icons/hi";
 
 export default function Sidebar() {
     console.log(process.env)
     const {data: session} = useSession();
+    console.log(session)
   return (
-    <div className='flex flex-col gap-4 p-3'>
+    <div className='flex flex-col p-3 justify-between h-screen'>
+        <div className="flex flex-col gap-4 p-3">
         <Link href={'/'}>
             <FaXTwitter className='w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200' />
         </Link>
@@ -29,9 +31,19 @@ export default function Sidebar() {
     console.log("sign in clicked");
     signIn()
 }} className="bg-blue-400 font-semibold text-white rounded-full px-4 py-2 mt-4 hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline">Sign In</button>
-
-    )
-}
+)}
+</div>
+{
+    session && (
+        <div className="text-gray-700 text-sm flex items-center cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200">
+            <img src={session.user.image} alt="img" className='h-10 w-10 rounded-full xl:mr-2 ' />
+            <div className="hidden xl:inline">
+                <h4 className='font-bold'>{session.user.name}</h4>
+                <p className='text-gray-500'>@{session.user.username}</p>
+            </div>
+            <HiDotsHorizontal className='h-5 xl:ml-8 hidden xl:inline ' />
+        </div>
+    )}
     </div>
   )
 }
