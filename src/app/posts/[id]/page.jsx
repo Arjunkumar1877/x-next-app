b92fe572d5ae825a6aa8dbcbe40d80140c17c3b4
Arjun kumar vs <arjun.tech177@gmail.com>
2024-05-17 12:52:01 +0530

    updated post page componenta nd fetched data from firebase to show post details

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import Post from '@/app/components/Post';
+import { app } from '@/app/firebase';
+import { getDoc,doc, getFirestore } from 'firebase/firestore';
+import Link from 'next/link';
+import React from 'react';
+import { HiArrowLeft } from 'react-icons/hi';
 
-export default function page() {
+export default async function postPage({params}) {
+  let data = {};
+  let id = params.id;
+  const db = getFirestore(app)
+ const querySnapshot = await getDoc(doc(db, 'posts', params.id));
+ data = { ...querySnapshot.data(), id: querySnapshot.id};
   return (
-    <div>page</div>
+    <div className='max-w-xl mx-auto border-r border-l min-h-screen '>
+      <div className="flex items-center space-x-2 py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
+        <Link href={'/'} className='hover:bg-gray-100 rounded-full p-2'>
+         <HiArrowLeft className='h-5 w-5 ' />
+        </Link>
+        <h2 className='sm:text-lg '>Back</h2>
+      </div>
+      <Post post={data} id={data.id} />
+    </div>
   )
 }
