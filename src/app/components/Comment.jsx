6a8339c1b2efd59a132078f3dac5904b8cd2c0d4
Arjun kumar vs <arjun.tech177@gmail.com>
2024-05-18 04:26:24 +0530

    added like comment functionality

--- src/app/components/Comment.jsx
+++ src/app/components/Comment.jsx
@@ -1,7 +1,49 @@
-import React from 'react'
-import { HiDotsHorizontal } from 'react-icons/hi'
+import { collection, deleteDoc, doc, getFirestore, onSnapshot, serverTimestamp, setDoc } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react'
+import { HiDotsHorizontal, HiHeart, HiOutlineHeart } from 'react-icons/hi'
+import { app } from '../firebase';
+import { useSession } from 'next-auth/react';
 
-export default function Comment({comment, id}) {
+export default function Comment({comment, commentId, originalPostId}) {
+    const [isLiked, setIsLiked] = useState(false);
+    const [likes, setLikes] = useState([]);
+    const db = getFirestore(app);
+    const {data: session} =  useSession();
+
+    const likePost = async () => {
+        console.log("clicked liking")
+        if (session) {
+            if (isLiked) {
+              deleteDoc(doc(db, "posts", originalPostId, "comments", commentId, 'likes', session?.user?.uid));
+            } else {
+              await setDoc(doc(db, "posts", originalPostId, "comments", commentId, 'likes', session.user.uid), {
+                username: session.user.username,
+                timestamp: serverTimestamp(),
+              });
+            }
+          } else {
+            signIn();
+          }
+          
+      };
+
+  
+
+    useEffect(() => {
+        onSnapshot(collection(db, "posts", originalPostId, 'comments', commentId, "likes"), (snapshot) => {
+          setLikes(snapshot.docs);
+        });
+      }, [db]);
+    
+    useEffect(() => {
+        setIsLiked(
+          likes.findIndex((like) => like.id === session?.user?.uid) !== -1
+        );
+      }, [likes]);
+
+    console.log(isLiked)
+
+    
   return (
     <div className='flex p-3 border-b border-gray-200 hover:bg-gray-100 pl-10'>
         <img src={comment?.userImage} alt="user-img" className='h-9 w-9 rounded-full mr-4 '/>
@@ -20,6 +62,28 @@ export default function Comment({comment, id}) {
 
 <p className="text-gray-800 text-xs my-3 ">{comment?.comment}</p>
 
+<div className="flex items-center">
+{isLiked ? (
+          <HiHeart
+            onClick={likePost}
+            className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 text-red-600 hover:text-red-500 hover:bg-red-100"
+          />
+        ) : (
+          <HiOutlineHeart
+            onClick={likePost}
+            className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100"
+          />
+        )}
+{
+  likes.length > 0 && (
+    <span className={`text-xs ${isLiked && 'text-red-600'}`}>
+      {likes.length}
+    </span>
+  )
+}
+
+
+</div>
 
 
         </div>
--- src/app/components/Comments.jsx
+++ src/app/components/Comments.jsx
@@ -3,9 +3,11 @@ import React, { useEffect, useState } from 'react'
 import { app } from '../firebase';
 import { collection, getFirestore, onSnapshot, orderBy, query } from 'firebase/firestore';
 import Comment from './Comment';
+import { useSession } from 'next-auth/react';
 
 export default function Comments({id}) {
     const db = getFirestore(app);
+   
     const [comments, setComments] = useState([]);
     useEffect(()=>{
         onSnapshot(query(collection(db, 'posts', id, 'comments'), orderBy('timestamp', 'desc')), (snapshot)=>{
@@ -13,12 +15,15 @@ export default function Comments({id}) {
         })
 
      
-    }, [db, id])
+    }, [db, id]);
+
+  
+
   return (
     <div>
       {
         comments.map((comment)=> (
-            <Comment key={comment.id} comment={comment.data()} id={comment.id} />
+            <Comment key={comment.id} comment={comment.data()} commentId={comment.id} originalPostId={id}/>
         ))
       }
     </div>
